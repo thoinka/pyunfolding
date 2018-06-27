befc2e7cdee7e9f3ffcd05b31c3f7b94ec9c9ff3
Tobias Hoinka <thoinka@MacBook-Pro-3.fritz.box>
2018-06-27 16:09:32 +0200

    Various stuff, don't exactly remember.

--- test.py
+++ test.py
@@ -1,35 +1,167 @@
 import numpy as np
-import binning
-import model
-import likelihood as llh
-import solver
+from pyunfolding import binning
+from pyunfolding import model
+from pyunfolding import likelihood as llh
+from pyunfolding import solver
 from scipy.optimize import minimize
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
-from plot import corner_plot
+from pyunfolding.plot import corner_plot
 
-"""
-General structure of this module:
+class MinimizationResult:
+    def __init__(self, x, fun, jac, n_iter, success):
+        self.x = x
+        self.fun = fun
+        self.jac = jac
+        self.n_iter = n_iter
+        self.success = success
 
-Binning:
-    f(x) ---> f
-    g(y) ---> g
+    def __repr__(self):
+        return self.__str__()
 
-Model[Binning_x, Binning_y]:
-    Model ---f_test---> g_test
+    def __str__(self):
+        s = "Minimization Result:\n"
+        s += "--------------------\n"
+        s += "x:       {}\n".format(self.x)
+        s += "fun:     {}\n".format(self.fun)
+        s += "jac:     {}\n".format(self.jac)
+        s += "n_iter:  {}\n".format(self.n_iter)
+        s += "success: {}\n".format(self.success)
+        return s
 
-Likelihood[Model]:
-    Likelihood ---g_obs, f_est---> llh
 
-Solver[Likelihood]:
-    Solver ---g_obs---> f_est
-"""
+def momentum_minimizer(fun,
+                       grad,
+                       x0,
+                       max_iter=1000,
+                       tol=1e-8,
+                       alpha=0.03,
+                       lr=0.1):
+    """Momentum Minimizer.
+
+    Parameters
+    ----------
+    fun : function
+          Function to be minimized.
+    grad : function
+           Jacobian of the fun.
+    x0 : float or numpy.array
+         Start values
+    max_iter : int, optional
+        Maximum number of iterations.
+    tol : float
+          Relative tolerance.
+    alpha : float
+            Momentum coefficient.
+    lr : float
+         Learning rate.
+
+    Returns
+    -------
+    result : MinimizationResult
+             The results.
+    """
+    x = [x0]
+    delta_x = 0.0
+    if grad is None:
+        def grad(x): return num_gradient(fun, x)
+
+    for i in range(max_iter):
+        g = grad(x[-1])
+        x += [x[-1] - lr * g - alpha * delta_x]
+        delta_x = x[-1] - x[-2]
+        if np.linalg.norm(g) < tol:
+            return MinimizationResult(x=x[-1], fun=fun(x[-1]), jac=grad(x[-1]),
+                                      n_iter=i, success=True), np.array(x)
+    return MinimizationResult(x=x[-1], fun=fun(x[-1]), jac=grad(x[-1]),
+                              n_iter=max_iter, success=False), np.array(x)
 
 
-def condition(A):
-    u, s, v = np.linalg.svd(A)
-    # return np.max(s) / np.min(s)
-    return np.max(s[1:-1]) / np.linalg.norm(s[1:-1])
+def rmsprop_minimizer(fun,
+                      grad,
+                      x0,
+                      max_iter=1000,
+                      tol=1e-8,
+                      gamma=0.99,
+                      mu=0.0,
+                      lr=0.1):
+    """RMSProp Minimizer.
+
+    Parameters
+    ----------
+    fun : function
+          Function to be minimized.
+    grad : function
+           Jacobian of the fun.
+    x0 : float or numpy.array
+         Start values
+    max_iter : int, optional
+        Maximum number of iterations.
+    tol : float
+          Relative tolerance.
+    gamma : float
+            Forgetting factor.
+    mu : float
+         Additional momentum.
+    lr : float
+         Learning rate.
+
+    Returns
+    -------
+    result : MinimizationResult
+             The results.
+    """
+    x = [x0]
+    v = 0.0
+    m = 0.0
+    if grad is None:
+        def grad(x): return num_gradient(fun, x)
+
+    for i in range(max_iter):
+        g = grad(x[-1])
+        v = gamma * v + (1.0 - gamma) * g ** 2
+        m = mu * m + lr * g / (np.sqrt(v) + 1e-8)
+        x += [x[-1] - m]
+        if np.linalg.norm(g) < tol:
+            return MinimizationResult(x=x[-1], fun=fun(x[-1]), jac=grad(x[-1]),
+                                      n_iter=i, success=True), np.array(x)
+    return MinimizationResult(x=x[-1], fun=fun(x[-1]), jac=grad(x[-1]),
+                              n_iter=max_iter, success=False), np.array(x)
+
+
+def num_gradient(fun, x0, eps=1e-4):
+    """ Evaluates the gradient of fun at x0 numerically.
+
+    Parameters
+    ----------
+    fun : function
+          Function to evaluate the gradient for.
+    x0 : numpy.array
+         Point at which to evaluate the gradient for.
+    eps : float
+          step size.
+
+    Returns
+    -------
+    gradient : numpy.array
+               The gradient.
+    """
+    gradient = []
+    f0 = fun(x0)
+    T = np.tile(x0, (len(x0), 1)) + np.eye(len(x0)) * eps
+    for i in range(len(x0)):
+        gradient.append((fun(T[i, :]) - f0) / eps)
+    return np.array(gradient)
+
+
+def paraboloid_fit(x, f):
+    n_dim = x.shape[1]
+    def paraboloid(x, params):
+        return np.sum(x.T * np.matmul(params, x.T), axis=0)
+    F = lambda p: np.sum((f - paraboloid(x, p.reshape(n_dim, n_dim))) ** 2)
+    result = minimize(F, x0=np.eye(n_dim).flatten(), tol=1e-10)
+    print(result)
+    return result.x.reshape(n_dim, n_dim)
 
 
 if __name__ == "__main__":
@@ -49,61 +181,50 @@ if __name__ == "__main__":
     train_samples_2 = np.random.normal(loc=train_samples, scale=fuzziness)
     train_samples = np.vstack((train_samples, train_samples_2)).T
 
-    b = 40
+    b = 400
 
     binning_tgt = binning.GridBinning([10])
     binning_tgt.fit(test_y)
-    binning_obs = binning.TreeBinning(100, min_samples_leaf=b)
+    bin_ens = [binning.TreeBinning(50, min_samples_leaf=b) for _ in range(3)]
+    binning_obs = binning.EnsembleBinning(*bin_ens)
+    #binning_obs = binning.TreeBinning(100, min_samples_leaf=b)
     f_class = binning_tgt.digitize(test_y)
     f_true = binning_tgt.histogram(test_y)
     binning_obs.fit(test_samples, f_class)
 
     m = model.Model(binning_obs, binning_tgt)
     m.fit(test_samples, test_y)
-    print(b, condition(m.A))
-    mx, my = np.meshgrid(np.linspace(0.0, 1.0, len(f_true)), np.linspace(0.0, 1.0, len(f_true)))
-    C = np.exp(-(mx - my) ** 2 / (2.0 * 0.3 ** 2))
-    #C = np.eye(len(f_true)) + np.roll(np.eye(len(f_true)),1) + np.roll(np.eye(len(f_true)),-1)
-    #C[0,0] = C[-1,-1] = 1.0
 
-    C = np.linalg.pinv(C)
-
-    print(C.shape)
-    """
-    likelihood = Likelihood(LeastSquares + TikhonovRegularization + OnlyPositive, m)
-    """
     likelihood = llh.Likelihood(m)
-    likelihood.append(llh.LeastSquares())
-    likelihood.append(llh.TikhonovRegularization2(1e-5, C=C))
-    #likelihood.append(llh.OnlyPositive(0.01))
+    likelihood.append(llh.Poisson())
+    #likelihood.append(llh.TikhonovRegularization(1e-1))
 
-    # llh.TikhonovRegularization(model,0.0)
     x0 = m.x0(train_samples, pos=True, tau=1e-3)
-    #sol = solver.Minimizer(likelihood, gradient=True)
-    #min_res = sol.solve(f_true, train_samples)
-    sol = solver.MCMCSolver(likelihood)
-    print(f_true)
-    
-    #x0 = np.ones_like(x0)
-    #x0 += np.random.randn(len(x0)) * 5.0
+    sol = solver.NewtonMinimizer(likelihood)
+    result = sol.solve(x0, train_samples)
+    g = binning_obs.histogram(train_samples)
 
-    print(x0)
-    result = sol.solve(f_true, train_samples, n_iter=1000000, pass_samples=True)
+    print("x0", x0)
+    print("res", result.f)
+
+    print(num_gradient(lambda f: likelihood(f, g), result.f))
+    print(result)
+    
+    
+    sol = solver.MCMCSolver(likelihood)
+    result = sol.solve(result.f, train_samples, verbose=True,
+                       pass_samples=True, n_iter=5000000,
+                       error_method="central", value_method="median")
+    g = binning_obs.histogram(train_samples)
+    
+    corner_plot(result.samples_x[::100,4:7])
+    plt.show()
+
+    print(num_gradient(lambda f: likelihood(f, g), result.f))
     print(result)
 
-    #corner_plot(result.samples_x[::100,5:8])
-    #plt.show()
-
-    #print(np.sqrt(1.0 / likelihood.hess(result.f, m.predict_g(train_samples)).diagonal()))
-    #print(min_res.f_err)
-    print(np.sum(result.f_err, axis=0))
-    hist = np.vstack((
-        np.tile(np.arange(len(result.f)), (len(result.samples_x),1)).flatten(),
-        result.samples_x.flatten()
-    ))
-    plt.hist2d(*hist, bins=(np.arange(len(result.f) + 1) - 0.5, 100), norm=LogNorm())
-
     plt.plot(binning_tgt.histogram(train_y), drawstyle="steps-mid", color="k")
-    plt.errorbar(np.arange(len(result.f)), result.f, result.f_err, 0.5 * np.ones(len(result.f)), linestyle="", color="k")
-    plt.ylim([-50,None])
+    plt.errorbar(np.arange(len(result.f))[1:-1], result.f[1:-1],
+                 result.f_err[:,1:-1], 0.5 * np.ones(len(result.f[1:-1])),
+                 linestyle="", color="k")
     plt.show()
