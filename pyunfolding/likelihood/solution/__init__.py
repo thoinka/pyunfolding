21d1fee8695c519ebf2a3e94031ba485d7e170fe
Tobias Hoinka <thoinka@MacBook-Pro-3.fritz.box>
2019-08-08 14:18:36 +0200

    Removed newtons method

--- pyunfolding/likelihood/solution/__init__.py
+++ pyunfolding/likelihood/solution/__init__.py
@@ -1,9 +1,7 @@
 from .base import SolutionBase
 from .mcmc import MCMC
 from .minimizer import Minimizer
-from .newton import NewtonMinimizer
 
 __all__ = ("SolutionBase",
            "MCMC",
-           "Minimizer",
-           "NewtonMinimizer")
+           "Minimizer")
